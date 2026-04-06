1da8e84352455321a489234cdb67eafc39c88268
Rafael Nakayama <RafaelNakayama2003@gmail.com>
2026-04-06 16:49:25 -0300

    feat(api): create loop_matches function

--- ingestion/fetch_matches.py
+++ ingestion/fetch_matches.py
@@ -1,21 +1,45 @@
 import requests
 import json
 
-league = 'bl1'
-seasons = 2006
-matchday = 1
+seasons = range(2006, 2027)
+matchday = range(1,35)
 
-def create_url(league_param, seasons_param, matchday_param):
+def create_url(seasons_param, matchday_param):
 
     """
     This function generates the url to gather data from
-    the website
+    the website, it takes one day at a time.
     """
 
-    url = f'https://api.openligadb.de/getmatchdata/{league_param}/{seasons_param}/{matchday_param}'
+    try:
+        url = f'https://api.openligadb.de/getmatchdata/bl1/{seasons_param}/{matchday_param}'
+        r = requests.get(url)
 
-    r = requests.get(url)
+    except requests.RequestException:
+        print("Error: Invalid value on either season/league")
+    
+    else:
+        return r.content
 
-    return print(r.content)
+def loop_matches(seasons_loop, matchday_loop):
 
-create_url(league, seasons, matchday)
+    """
+    This function loops through the matchdays and seasons
+    going all the way to select all data
+    """
+
+    try:
+        store_data = []
+
+        for i in seasons_loop:
+            for j in matchday_loop:
+                day = create_url(i, j)
+                store_data.append(day)
+
+    except requests.RequestException:
+        print("Error: Connection failed.")
+
+    else:
+        return store_data
+    
+loop_matches(seasons, matchday)
