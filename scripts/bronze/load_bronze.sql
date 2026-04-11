25fd17d90e51b6cef57f8a1df760e20394ee5f1e
Rafael Nakayama <RafaelNakayama2003@gmail.com>
2026-04-11 15:26:12 -0300

    feat: add select and with clauses

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -9,3 +9,42 @@ FROM OPENROWSET (
     SINGLE_CLOB
 ) AS src;
 
+SELECT
+    matchID,
+    matchDateTime,
+    timeZoneID,
+    leagueId,
+    leagueName,
+    leagueSeason,
+    leagueShortcut,
+    matchDateTimeUTC,
+    [group],
+    team1,
+    team2,
+    lastUpdateDateTime,
+    matchIsFinished,
+    matchResults,
+    goals,
+    [location],
+    numberOfViewers
+FROM OPENJSON(@json)
+
+WITH (
+    matchID INT,
+    matchDateTime DATETIME,
+    timeZoneID VARCHAR(50),
+    leagueId INT,
+    leagueName NVARCHAR(75),
+    leagueSeason INT,
+    leagueShortcut VARCHAR(50),
+    matchDateTimeUTC DATETIME,
+    [group] NVARCHAR(MAX) AS JSON,
+    team1 NVARCHAR(MAX) AS JSON,
+    team2 NVARCHAR(MAX) AS JSON,
+    lastUpdateDateTime DATETIME,
+    matchIsFinished BIT,
+    matchResults NVARCHAR(MAX) AS JSON,
+    goals NVARCHAR(MAX) AS JSON,
+    [location] NVARCHAR(MAX) AS JSON,
+    numberOfViewers INT
+)
