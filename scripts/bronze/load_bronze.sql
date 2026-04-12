441d222add5fa60213ca4631c0e03ceac9a11232
Rafael Nakayama <RafaelNakayama2003@gmail.com>
2026-04-12 07:44:51 -0300

    fix: repair 'load_bronze' structure, loose select removed

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -2,7 +2,7 @@ USE dw_hgg_database
 
 BEGIN TRAN
 
-DECLARE @json NVARCHAR(MAX); -- TRANSFORM THE .CSV FILE INTO A HUGE STRING
+DECLARE @json NVARCHAR(MAX); -- TRANSFORM THE .JSON FILE INTO A HUGE STRING
 
 SELECT @json = BulkColumn
 
@@ -10,46 +10,6 @@ FROM OPENROWSET (
     BULK 'C:\Users\Rafae\Projetos\dw_huggingface\datasets\dataframe.json',
     SINGLE_CLOB
 ) AS src;
-
-SELECT
-    matchID,
-    matchDateTime,
-    timeZoneID,
-    leagueId,
-    leagueName,
-    leagueSeason,
-    leagueShortcut,
-    matchDateTimeUTC,
-    [group],
-    team1,
-    team2,
-    lastUpdateDateTime,
-    matchIsFinished,
-    matchResults,
-    goals,
-    [location],
-    numberOfViewers
-FROM OPENJSON(@json)
-
-WITH (
-    matchID INT,
-    matchDateTime DATETIME,
-    timeZoneID VARCHAR(50),
-    leagueId INT,
-    leagueName NVARCHAR(75),
-    leagueSeason INT,
-    leagueShortcut VARCHAR(50),
-    matchDateTimeUTC DATETIME,
-    [group] NVARCHAR(MAX) AS JSON,
-    team1 NVARCHAR(MAX) AS JSON,
-    team2 NVARCHAR(MAX) AS JSON,
-    lastUpdateDateTime DATETIME,
-    matchIsFinished BIT,
-    matchResults NVARCHAR(MAX) AS JSON,
-    goals NVARCHAR(MAX) AS JSON,
-    [location] NVARCHAR(MAX) AS JSON,
-    numberOfViewers INT
-)
 
 INSERT INTO bronze.dataframe (
     matchID,
@@ -92,5 +52,25 @@ SELECT
     numberOfViewers
 
 FROM OPENJSON(@json)
+
+WITH (
+    matchID INT,
+    matchDateTime DATETIME,
+    timeZoneID VARCHAR(50),
+    leagueId INT,
+    leagueName NVARCHAR(75),
+    leagueSeason INT,
+    leagueShortcut VARCHAR(50),
+    matchDateTimeUTC DATETIME,
+    [group] NVARCHAR(MAX) AS JSON,
+    team1 NVARCHAR(MAX) AS JSON,
+    team2 NVARCHAR(MAX) AS JSON,
+    lastUpdateDateTime DATETIME,
+    matchIsFinished BIT,
+    matchResults NVARCHAR(MAX) AS JSON,
+    goals NVARCHAR(MAX) AS JSON,
+    [location] NVARCHAR(MAX) AS JSON,
+    numberOfViewers INT
+)
 
 COMMIT
