aabef268ec6f6febc4ccfa1c3909d75ca717e290
Rafael Nakayama <RafaelNakayama2003@gmail.com>
2026-04-19 10:21:20 -0300

    feat: populate silver.location table

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -16,12 +16,35 @@ SELECT DISTINCT
     groupID
 
 FROM bronze.dataframe
-CROSS APPLY OPENJSON([group]) WITH (
-    groupName VARCHAR(50),
+CROSS APPLY OPENJSON([group]) WITH ( -- applies a function to eahc row individually,
+    groupName VARCHAR(50), -- for each row in the bronze table , take the group column and pass it to OPENJSONN
     groupOrderId INT,
     groupID INT
 )
 
-COMMIT
+---------------------- (silver.location) --------------------------
 
----------------------- (silver.groups) --------------------------
+INSERT INTO silver.[location] (
+    location_id,
+    location_city,
+    location_stadium
+)
+
+SELECT DISTINCT
+    locationID,
+    locationCity,
+    locationStadium
+
+FROM bronze.dataframe
+
+CROSS APPLY OPENJSON([location]) WITH (
+    locationID INT,
+    locationCity VARCHAR(50),
+    locationStadium VARCHAR(50)
+)
+
+---------------------- (silver.teams) --------------------------
+
+
+
+COMMIT
