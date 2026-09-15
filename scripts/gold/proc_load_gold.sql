c75fa5ea01bb86b76366777ebd0bd3fc097e4d0e
rafaelnakayama <rafaelnakayama2003@gmail.com>
2026-09-15 12:06:03 -0300

    feat: add gold.fact_matches with silver.match_results inner join

--- scripts/gold/proc_load_gold.sql
+++ scripts/gold/proc_load_gold.sql
@@ -22,38 +22,6 @@ BEGIN
     IF OBJECT_ID ('gold.dim_teams', 'U') IS NOT NULL
         DROP TABLE gold.dim_teams;
 
-    -- Create and insert columns into fact_matches
-
-    CREATE TABLE gold.fact_matches (
-        match_id INT PRIMARY KEY,
-        league_name NVARCHAR(75),
-        league_season INT,
-        match_date_time_utc DATETIME,
-        match_is_finished BIT,
-        number_of_viewers INT
-    )
-
-    INSERT INTO gold.fact_matches (
-        match_id,
-        league_name,
-        league_season,
-        match_date_time_utc,
-        match_is_finished,
-        number_of_viewers
-    )
-
-    SELECT 
-        match_id,
-        league_name,
-        league_season,
-        match_date_time_utc,
-        match_is_finished,
-        number_of_viewers
-
-    FROM silver.matches
-
-    WHERE match_is_finished = 1
-
     -- Create and insert columns into dim_teams (dim_teams is a copy of silver.teams)
 
     CREATE TABLE gold.dim_teams (
@@ -77,6 +45,65 @@ BEGIN
         team_icon_url
 
     FROM silver.teams
+
+    -- Create and insert columns into fact_matches
+
+    CREATE TABLE gold.fact_matches (
+        match_id INT PRIMARY KEY,
+        group_id INT,
+        team1_id INT,
+        team2_id INT,
+        league_name NVARCHAR(75),
+        league_season INT,
+        match_date_time_utc DATETIME,
+        number_of_viewers INT,
+        goals_team1 INT,
+        goals_team2 INT,
+        total_goals INT,
+        winner_team_id INT
+    )
+
+    INSERT INTO gold.fact_matches (
+        match_id,
+        group_id,
+        team1_id,
+        team2_id,
+        league_name,
+        league_season,
+        match_date_time_utc,
+        number_of_viewers,
+        goals_team1,
+        goals_team2,
+        total_goals,
+        winner_team_id
+    )
+
+    SELECT 
+        MT.match_id,
+        MT.group_id,
+        MT.team1_id,
+        MT.team2_id,
+        MT.league_name,
+        MT.league_season,
+        MT.match_date_time_utc,
+        MT.number_of_viewers,
+        MR.points_team1 AS goals_team1,
+        MR.points_team2 AS goals_team2,
+        (MR.points_team1 + MR.points_team2) AS total_goals,
+
+    CASE 
+        WHEN MR.points_team1 > MR.points_team2
+            THEN team1_id
+        WHEN MR.points_team1 < MR.points_team2
+            THEN team2_id
+        ELSE NULL
+    END AS winner_team_id
+
+    FROM silver.matches AS MT
+    INNER JOIN silver.match_results AS MR
+    ON MT.match_id = MR.match_id
+
+    WHERE MT.match_is_finished = 1 AND MR.result_type_id = 2
     
     COMMIT
 
